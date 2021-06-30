894af3a1da182d25a3d0ffaf634af3c76c40c0a4
PRCatarino <paulo061104@gmail.com>
2021-06-30 10:41:37 -0300

    busca-celebridades e filmes

--- js/painel.js
+++ js/painel.js
@@ -20,7 +20,6 @@ function busca(){
             const dados = await resposta.json();
             
             for(let i in dados){                  // pode ser feita assim tbm for(let i=0; i<dados.leght;i++)
-                console.log(dados[i])
                 let item = null;
 
                 if(tipoBusca =="a"){
@@ -44,7 +43,7 @@ function busca(){
                     <p>
                         ${nome}
                     </p>
-                    <a href="/">
+                    <a href="javascript:abrirPopUp(${item.id})">
                         <img class="imagemDaBusca" src="${imagem}"/>
                     </a>
                 </div>`                
@@ -53,4 +52,33 @@ function busca(){
     })
 }
 
+function abrirPopUp(id){
+    document.querySelector("#popUp").style.display ='flex'
+    const tipoBusca = document.querySelector('input[name="tipoBusca"]:checked').value;
+    let url = ''
+    if(tipoBusca == "a"){
+        url = `https://api.tvmaze.com/people/${id}`;
+    }else{
+        url = `https://api.tvmaze.com/shows/${id}`;
+    }
+    fetch(url).then(async(resposta)=>{
+        if(resposta.ok){
+            const dados = await resposta.json();
+            document.querySelector(".image").innerHTML = `<img class="imagemCentral" src="${dados.image.medium}"/>`
+            document.querySelector(".nomeDoFilmeArtista").innerHTML= `${dados.name}`
+            document.querySelector(".sumario").innerHTML = `<h3>${dados.summary}</h3>`
+            console.log(dados)
+            
+        }
+    })
+    fetch("http://localhost:3002/buscarPessoa",{method:'POST'}).then(async(resposta)=>{
+    console.log(await resposta.json())
+    })
+ 
+
+}
+
+function fecharPopUp(){
+    document.querySelector("#popUp").style.display = 'none'
+}
 
